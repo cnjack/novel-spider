86240b5ac0165110d21c46d93132c99d625fb8db
jack <h_7357@qq.com>
2016-11-11 13:26:27 +0800

    添加搜索\n目测有bug get百度服务器容易EOF

--- main.go
+++ main.go
@@ -2,17 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"git.oschina.net/cnjack/novel-spider/spider"
 )
 
 func main() {
-	s := &spider.SnwxChapter{}
-	s.Match("http://www.snwx.com/book/124/124785/26045253.html")
-	resp, err := s.Gain()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(resp)
+	fmt.Println("testing")
 }
---
+++ spider/snwxchapter_test.go
@@ -0,0 +1,28 @@
+package spider_test
+
+import (
+	"testing"
+
+	"git.oschina.net/cnjack/novel-spider/spider"
+	"github.com/stretchr/testify/assert"
+)
+
+const testChapterUrl = "http://www.snwx.com/book/0/381/155205.html"
+
+func TestSnwxChapter_Match(t *testing.T) {
+	s := spider.SnwxChapter{}
+	b := s.Match(testChapterUrl)
+	assert.Equal(t, true, b)
+}
+
+func TestSnwxChapter_Gain(t *testing.T) {
+	s := spider.SnwxChapter{}
+	b := s.Match(testChapterUrl)
+	chapter, err := s.Gain()
+	chapterString, b2 := chapter.(string)
+	if assert.NoError(t, err) {
+		assert.Equal(t, true, b)
+		assert.Equal(t, true, b2)
+		assert.NotEmpty(t, chapterString)
+	}
+}
