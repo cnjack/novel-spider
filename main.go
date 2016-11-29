5e736b60249b6e2537cc9336bd28d85fe27d8ba4
jack <h_7357@qq.com>
2016-11-29 11:20:03 +0800

    调试添加新能监控

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 
-	"git.oschina.net/cnjack/novel-spider/job"
 	"git.oschina.net/cnjack/novel-spider/httpserv"
+	"git.oschina.net/cnjack/novel-spider/job"
 )
 
 func main() {
