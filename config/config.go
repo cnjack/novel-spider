58a8731ebfd7202aa062c6c5504acb6f1f366197
jack <h_7357@qq.com>
2017-10-11 15:22:51 +0800

    config

--- config/config.go
+++ config/config.go
@@ -9,36 +9,20 @@ var ConfigPath = "./config.ini"
 var config *goconfig.ConfigFile
 
 type HttpConfig struct {
-	Port       string
-	AccessKey  string
-	SecretKey  string
-	BucketName string
-	BucketUrl  string
+	Port string
 }
 
 type MysqlConfig struct {
 	DSN string
 }
 
-type SpiderConfig struct {
-	BrokerAddr  []string
-	BrokerTopic string
-	BrokerChannel string
-	MaxProcess  int
-}
-
 var (
 	h = &HttpConfig{}
-	s = &SpiderConfig{}
 	m = &MysqlConfig{}
 )
 
 func GetHttpConfig() *HttpConfig {
 	return h
-}
-
-func GetSpiderConfig() *SpiderConfig {
-	return s
 }
 
 func GetMysqlConfig() *MysqlConfig {
@@ -51,36 +35,10 @@ func load() {
 	if err != nil {
 		h.Port = ":1314"
 	}
-	h.AccessKey, _ = config.GetValue("http", "accessKey")
-	h.SecretKey, _ = config.GetValue("http", "secretKey")
-	h.BucketName, _ = config.GetValue("http", "bucketName")
-	h.BucketUrl, _ = config.GetValue("http", "bucketHost")
-	s.BrokerAddr = config.MustValueArray("spider", "broker_addrs", ",")
-	s.MaxProcess, err = config.Int("spider", "max_process")
-	if err != nil {
-		s.MaxProcess = 50
-	}
-	s.BrokerTopic, err = config.GetValue("spider", "broker_topic")
-	if err != nil {
-		s.BrokerTopic = "novel-task"
-	}
-	s.BrokerChannel, err = config.GetValue("spider", "broker_channel")
-	if err != nil {
-		s.BrokerChannel = "novel-task-channel"
-	}
 	m.DSN, err = config.GetValue("mysql", "dsn")
 	if err != nil {
 		m.DSN = "root:root@tcp(127.0.0.1:3306)/novel?charset=utf8&parseTime=True&loc=Local"
 	}
-}
-
-func ReloadConfig() {
-	var err error
-	config, err = goconfig.LoadConfigFile(ConfigPath)
-	if err != nil {
-		config = &goconfig.ConfigFile{}
-	}
-	load()
 }
 
 func init() {
