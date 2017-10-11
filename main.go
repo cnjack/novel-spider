3943ed085a7520b2c9a78d65d71a922d1711498e
jack <h_7357@qq.com>
2017-10-11 15:29:00 +0800

    add some init func

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"git.oschina.net/cnjack/novel-spider/api"
+	"git.oschina.net/cnjack/novel-spider/model"
 )
+
+func init() {
+	log.SetOutput(os.Stdout)
+
+	db, _ := model.MustGetDB()
+	db.LogMode(true)
+	if err := db.CreateTable(&model.Novel{}).Error; err != nil {
+		log.Println(db)
+	}
+}
 
 func main() {
 	api.Start()
--- model/db_test.go
+++ model/db_test.go
@@ -6,23 +6,6 @@ import (
 	"git.oschina.net/cnjack/novel-spider/model"
 )
 
-func TestMustGetDB(t *testing.T) {
-	db, err := model.MustGetDB()
-	if err != nil {
-		t.Error(err)
-	}
-	task := &model.Task{
-		TType:  model.NovelTask,
-		Url:    "http://www.snwx.com/book/0/381/",
-		Status: model.TaskStatusPrepare,
-		Times:  -1,
-	}
-	c := db.Model(task).Create(task)
-	if c.Error != nil {
-		t.Error(c.Error)
-	}
-}
-
 func TestInitDB(t *testing.T) {
 	err := model.InitDB()
 	if err != nil {
