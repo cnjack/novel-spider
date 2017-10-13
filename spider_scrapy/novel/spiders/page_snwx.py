ea528c9d16369c7ac03b64b17be308013473df0b
jack <h_7357@qq.com>
2017-10-13 10:46:22 +0800

    snwx down

--- spider_scrapy/novel/spiders/page_snwx.py
+++ spider_scrapy/novel/spiders/page_snwx.py
@@ -27,7 +27,7 @@ class NovelSpider(CrawlSpider):
     cover_xpath = '//div[@id="fmimg"]/img/@src'
     style_xpath = '//div[@class="infotitle"]/i[2]/text()'
     status_xpath = '//div[@class="infotitle"]/i[3]/text()'
-    intro_xpath = '//div[@class="intro"]/text()'
+    intro_xpath = '//div[@class="intro"]'
     chapters = '//div[@id="list"]/dl/dd/a'
 
     def parse_item(self, response):
@@ -35,35 +35,35 @@ class NovelSpider(CrawlSpider):
         item["url"] = response.url
         name = response.xpath(self.name_xpath).extract()
         if len(name) > 0:
-            item["name"] = name[0].encode('utf-8')
+            item["name"] = name[0]
         else:
             item["name"] = ""
         auth_label = response.xpath(self.auth_xpath).extract()
         if len(auth_label) > 0:
-            item['auth'] = filter.get_label_value(auth_label[0].encode('utf-8'))
+            item['auth'] = filter.get_label_value(auth_label[0])
         else:
             item['auth'] = ""
         covers = response.xpath(self.cover_xpath).extract()
         if len(covers) > 0:
-            item['cover'] = covers[0].encode('utf-8')
+            item['cover'] = covers[0]
         else:
             item['cover'] = ""
         style_label = response.xpath(self.style_xpath).extract()
         if len(style_label) > 0:
-            item['style'] = filter.get_label_value(style_label[0].encode('utf-8'))
+            item['style'] = filter.get_label_value(style_label[0])
         else:
             item['style'] = ""
 
         status_label = response.xpath(self.status_xpath).extract()
         if len(status_label) > 0:
-            item['status'] = filter.get_label_value(status_label[0].encode('utf-8')).decode().replace("小说", "")
+            item['status'] = filter.get_label_value(status_label[0]).replace("小说", "")
         else:
             item['status'] = ""
 
         intros = response.xpath(self.intro_xpath).extract()
         if len(intros) > 0:
-            intro = intros[0].encode('utf-8').decode()
-            item['intro'] = re.compile(r'<b>[^<]+</b>', re.S).sub("", intro)
+            intro = re.compile(r'<[^>]*br[^>]*>', re.S).sub("\n", intros[0])
+            item['intro'] = re.compile(r'<[^>]+>', re.S).sub("", intro).strip()
         else:
             item['intro'] = ""
         return item
